b1761674a503dc4f5e4ad687cae51fdd6736f2c4
patryk <oksikpatryk@gmail.com>
2021-06-03 16:25:45 +0200

    added caching

--- app/src/main/java/com/oksik/github_app/network/RepositoryService.kt
+++ app/src/main/java/com/oksik/github_app/network/RepositoryService.kt
@@ -1,28 +1,25 @@
 package com.oksik.github_app.network
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import android.provider.SyncStateContract
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.oksik.github_app.model.CommitItem
 import com.oksik.github_app.model.Repository
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Deferred
+import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
+import java.io.IOException
 
 
 private const val BASE_URL = "https://api.github.com/"
-
-private val moshi = Moshi.Builder()
-    .add(KotlinJsonAdapterFactory())
-    .build()
-
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(MoshiConverterFactory.create(moshi))
-    .addCallAdapterFactory(CoroutineCallAdapterFactory())
-    .baseUrl(BASE_URL)
-    .build()
 
 interface GitHubApiService {
     @GET("repos/{owner}/{repository}")
@@ -35,11 +32,52 @@ interface GitHubApiService {
     fun getRepositoryCommits(
         @Path("owner") owner: String,
         @Path("repository") repository: String
-    ): Deferred<List<CommitItem>>
+    ): Deferred<List<CommitItem>?>?
 }
 
-object GitHubApi {
-    val RETROFIT_SERVICE: GitHubApiService by lazy {
-        retrofit.create(GitHubApiService::class.java)
+class RetrofitClient {
+
+    companion object {
+        fun create(context: Context): GitHubApiService {
+            val moshi = Moshi.Builder()
+                .add(KotlinJsonAdapterFactory())
+                .build()
+
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .client(okHttp(context))
+                .baseUrl(BASE_URL)
+                .build()
+
+            return retrofit.create(GitHubApiService::class.java)
+        }
+
+        private fun okHttp(context: Context): OkHttpClient {
+            val logging = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC)
+            val cacheSize = (5 * 1024 * 1024).toLong()
+            val myCache = Cache(context.cacheDir, cacheSize)
+            return OkHttpClient.Builder()
+                .cache(myCache)
+                .addInterceptor { chain ->
+                    var request = chain.request()
+                    request = if (hasNetwork(context)!!)
+                        request.newBuilder().header("Cache-Control", "public, max-age=" + 5).build()
+                    else
+                        request.newBuilder().header("Cache-Control", "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7).build()
+                    chain.proceed(request)
+                }
+                .addInterceptor(logging)
+                .build()
+        }
+
+        private fun hasNetwork(context: Context): Boolean? {
+            var isConnected: Boolean? = false // Initial Value
+            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+            if (activeNetwork != null && activeNetwork.isConnected)
+                isConnected = true
+            return isConnected
+        }
     }
 }
--- app/src/main/java/com/oksik/github_app/ui/main/MainViewModel.kt
+++ app/src/main/java/com/oksik/github_app/ui/main/MainViewModel.kt
@@ -1,18 +1,18 @@
 package com.oksik.github_app.ui.main
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import android.app.Application
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.*
 import com.oksik.github_app.model.CommitItem
 import com.oksik.github_app.model.Repository
-import com.oksik.github_app.network.GitHubApi
+import com.oksik.github_app.network.RetrofitClient
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-class MainViewModel : ViewModel() {
+class MainViewModel(private val context: Application) : AndroidViewModel(context) {
 
     private val _repository = MutableLiveData<Repository>()
     val repository: LiveData<Repository>
@@ -33,26 +33,32 @@ class MainViewModel : ViewModel() {
     fun search() {
         if (!owner.isNullOrEmpty() || !repositoryName.isNullOrEmpty()) {
             viewModelScope.launch(Dispatchers.IO) {
-                val repositoryCommits = getRepositoryCommits(owner!!, repositoryName!!)
-                val sortedRepositoryCommits = sortRepositoryCommitsByDateDesc(repositoryCommits)
-                _repository.postValue(getRepository(owner!!, repositoryName!!))
-                _commits.postValue(sortedRepositoryCommits)
+
+                try {
+                    val repositoryCommits = getRepositoryCommits(owner!!, repositoryName!!)
+                    val sortedRepositoryCommits =
+                        sortRepositoryCommitsByDateDesc(repositoryCommits!!)
+                    _repository.postValue(getRepository(owner!!, repositoryName!!))
+                    _commits.postValue(sortedRepositoryCommits!!)
+                } catch (e: Exception) {
+                    Log.e("tutaj",  "mes: ", e)
+                }
             }
         }
     }
 
     private suspend fun getRepository(owner: String, repositoryName: String): Repository {
-        val requestRepository = GitHubApi.RETROFIT_SERVICE.getRepository(owner, repositoryName)
+        val requestRepository = RetrofitClient.create(context).getRepository(owner, repositoryName)
         return requestRepository.await()
     }
 
-    private suspend fun getRepositoryCommits(owner: String, repositoryName: String): List<CommitItem> {
-        val requestCommits = GitHubApi.RETROFIT_SERVICE.getRepositoryCommits(owner, repositoryName)
-        return requestCommits.await()
+    private suspend fun getRepositoryCommits(owner: String, repositoryName: String): List<CommitItem>? {
+        val requestCommits = RetrofitClient.create(context).getRepositoryCommits(owner, repositoryName)
+        return requestCommits?.await()
     }
 
-    private fun sortRepositoryCommitsByDateDesc(repositoryCommits: List<CommitItem>): List<CommitItem> {
-        return repositoryCommits.sortedByDescending {
+    private fun sortRepositoryCommitsByDateDesc(repositoryCommits: List<CommitItem>?): List<CommitItem>? {
+        return repositoryCommits?.sortedByDescending {
             LocalDateTime.parse(
                 it.commit.author.date,
                 DateTimeFormatter.ISO_OFFSET_DATE_TIME
